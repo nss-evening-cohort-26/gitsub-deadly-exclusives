467a0d276211130a9dc08841adf57bf43178f8c4
Alex Berka <alexanderberka@gmail.com>
2024-02-05 21:09:45 -0600

    Set up data array

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Test");
+user = {
+  name: "Harrison Buford",
+  followers: 523,
+  following: 7,
+  favorites: [],
+  location: "Hog Wallow, KY",
+  email: "hbuford34@aol.com",
+  website: "harrisonisawesome34.com",
+  twitter: "",
+  highlights: [],
+  organizations: [],
+  repositories: [{
+    id: 1,
+    name: "",
+  }],
+  projects: [],
+  packages: []
+}
