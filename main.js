dba873409c8d9c8267717f87d4522ce507b5346f
fmings <mingsfelicia@gmail.com>
2024-02-05 21:32:56 -0600

    added the packages object data

--- main.js
+++ main.js
@@ -14,5 +14,41 @@ user = {
     name: "",
   }],
   projects: [],
-  packages: []
+  packages: [{
+    id: 1,
+    name: "Docker",
+    description: "A software platform used for building applications based on containers — small and lightweight execution environments.",
+    learnUrl: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-docker-registry",
+  },
+  {
+    id: 2,
+    name: "Apache Maven",
+    description: "A default package manager used for the Java programming language and the Java runtime environment.",
+    learnUrl: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-apache-maven-registry",
+  },
+  {
+    id: 3,
+    name: "NuGet",
+    description: "A free and open source package manager used for the Microsoft development platforms including .NET.",
+    learnUrl: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-nuget-registry",
+  },
+  {
+    id: 4,
+    name: "RubyGems",
+    description: "A standard format for distributing Ruby programs and libraries used for the Ruby programming language.",
+    learnUrl: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-rubygems-registry",
+  },
+  {
+    id: 5,
+    name: "npm",
+    description: "A package manager for JavaScript, included with Node.js. npm makes it easy for developers to share and reuse code.",
+    learnUrl: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-npm-registry",
+  },
+  {
+    id: 6,
+    name: "Containers",
+    description: "A single place for your team to manage Docker images and decide who can see and access your images.",
+    learnUrl: "https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry",
+  }],
+  
 }
