0e52bb7e71fea1f59e2145780489d6fa8edaf1bd
Derek Swann <148038820+Mrthincrisp@users.noreply.github.com>
2024-02-06 18:11:07 -0600

    project data

--- main.js
+++ main.js
@@ -13,6 +13,22 @@ user = {
     id: 1,
     name: "",
   }],
-  projects: [],
+  projects: [
+    {
+    id: 1,
+    name:"oh, Geez",
+    description: "I don't know what I'm doing."
+  },
+   {
+    id: 2,
+    name:"Panic inducing.",
+    description: "I can't get this to work."
+   },
+   {
+    id: 3,
+    name:"Last Project",
+    description: "If this doesn't work I quit."
+   }
+  ],
   packages: []
 }
